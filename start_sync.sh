bbf40d57530d3214e8fc9eaa0ffcf7fd91ea9d00
Denis Chilyakov <chilyakov@gmail.com>
2022-08-17 09:29:22 +0500

    Update start_sync.sh

--- start_sync.sh
+++ start_sync.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+dir="/mnt/pvedata"
+dst="node-engenr.sync"
+
 if [[ $# > 0 ]]; then
 
     while true
     do
-        for f in `find /mnt/pvedata -name *$1*raw -type f`
+        for f in `find $dir -name *$1*raw -type f`
         do
            #echo $f
-           /usr/bin/rsync -e "ssh -c aes128-gcm@openssh.com -o Compression=no -x" -B 128K -a --inplace --no-whole-file $f node-engenr.sync:/$f
+           /usr/bin/rsync -e "ssh -c aes128-gcm@openssh.com -o Compression=no -x" -B 128K -a --inplace --no-whole-file $f $dst:/$f
         done
 
         sleep 60
